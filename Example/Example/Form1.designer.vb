712951ae30428044c0eb45d17324fefcd89ffe9e
Sobatdata Software <sobatdata.software@gmail.com>
2019-08-08 10:07:38 +0700

    Init

---
+++ Example/Example/Form1.designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GunaDragControl1 = New Guna.UI.WinForms.GunaDragControl(Me.components)
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.GunaButton1 = New Guna.UI.WinForms.GunaButton()
+        Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaPictureBox1 = New Guna.UI.WinForms.GunaPictureBox()
+        Me.GunaVSeparator1 = New Guna.UI.WinForms.GunaVSeparator()
+        Me.GunaLabel2 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaLabel3 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaVSeparator2 = New Guna.UI.WinForms.GunaVSeparator()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaDragControl1
+        '
+        Me.GunaDragControl1.TargetControl = Me
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'GunaButton1
+        '
+        Me.GunaButton1.Animated = True
+        Me.GunaButton1.AnimationHoverSpeed = 0.07!
+        Me.GunaButton1.AnimationSpeed = 0.03!
+        Me.GunaButton1.BaseColor = System.Drawing.Color.White
+        Me.GunaButton1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaButton1.BorderSize = 2
+        Me.GunaButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.GunaButton1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaButton1.Image = Nothing
+        Me.GunaButton1.ImageSize = New System.Drawing.Size(20, 20)
+        Me.GunaButton1.Location = New System.Drawing.Point(120, 386)
+        Me.GunaButton1.Name = "GunaButton1"
+        Me.GunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaButton1.OnHoverBorderColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaButton1.OnHoverForeColor = System.Drawing.Color.White
+        Me.GunaButton1.OnHoverImage = Nothing
+        Me.GunaButton1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaButton1.Radius = 26
+        Me.GunaButton1.Size = New System.Drawing.Size(170, 54)
+        Me.GunaButton1.TabIndex = 0
+        Me.GunaButton1.Text = "DOWNLOAD"
+        Me.GunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GunaLabel1
+        '
+        Me.GunaLabel1.AutoSize = True
+        Me.GunaLabel1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.GunaLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaLabel1.Location = New System.Drawing.Point(21, 22)
+        Me.GunaLabel1.Name = "GunaLabel1"
+        Me.GunaLabel1.Size = New System.Drawing.Size(61, 19)
+        Me.GunaLabel1.TabIndex = 1
+        Me.GunaLabel1.Text = "Guna.UI"
+        '
+        'GunaPictureBox1
+        '
+        Me.GunaPictureBox1.BaseColor = System.Drawing.Color.White
+        Me.GunaPictureBox1.Image = CType(resources.GetObject("GunaPictureBox1.Image"), System.Drawing.Image)
+        Me.GunaPictureBox1.Location = New System.Drawing.Point(173, 90)
+        Me.GunaPictureBox1.Name = "GunaPictureBox1"
+        Me.GunaPictureBox1.Size = New System.Drawing.Size(64, 64)
+        Me.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.GunaPictureBox1.TabIndex = 2
+        Me.GunaPictureBox1.TabStop = False
+        '
+        'GunaVSeparator1
+        '
+        Me.GunaVSeparator1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator1.LineColor = System.Drawing.Color.Silver
+        Me.GunaVSeparator1.Location = New System.Drawing.Point(199, 270)
+        Me.GunaVSeparator1.Name = "GunaVSeparator1"
+        Me.GunaVSeparator1.Size = New System.Drawing.Size(11, 98)
+        Me.GunaVSeparator1.TabIndex = 3
+        '
+        'GunaLabel2
+        '
+        Me.GunaLabel2.AutoSize = True
+        Me.GunaLabel2.Font = New System.Drawing.Font("Segoe UI", 18.0!)
+        Me.GunaLabel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GunaLabel2.Location = New System.Drawing.Point(95, 211)
+        Me.GunaLabel2.Name = "GunaLabel2"
+        Me.GunaLabel2.Size = New System.Drawing.Size(222, 32)
+        Me.GunaLabel2.TabIndex = 4
+        Me.GunaLabel2.Text = "Guna.UI Framework"
+        '
+        'GunaLabel3
+        '
+        Me.GunaLabel3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.GunaLabel3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GunaLabel3.Location = New System.Drawing.Point(50, 258)
+        Me.GunaLabel3.Name = "GunaLabel3"
+        Me.GunaLabel3.Size = New System.Drawing.Size(312, 69)
+        Me.GunaLabel3.TabIndex = 5
+        Me.GunaLabel3.Text = "Custom controls, components and charts for C# and VB.NET WinForms with data bindi" &
+    "ng features"
+        Me.GunaLabel3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'GunaVSeparator2
+        '
+        Me.GunaVSeparator2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator2.LineColor = System.Drawing.Color.Silver
+        Me.GunaVSeparator2.Location = New System.Drawing.Point(199, 172)
+        Me.GunaVSeparator2.Name = "GunaVSeparator2"
+        Me.GunaVSeparator2.Size = New System.Drawing.Size(11, 71)
+        Me.GunaVSeparator2.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(407, 511)
+        Me.Controls.Add(Me.GunaLabel3)
+        Me.Controls.Add(Me.GunaLabel2)
+        Me.Controls.Add(Me.GunaVSeparator1)
+        Me.Controls.Add(Me.GunaPictureBox1)
+        Me.Controls.Add(Me.GunaLabel1)
+        Me.Controls.Add(Me.GunaButton1)
+        Me.Controls.Add(Me.GunaVSeparator2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GunaDragControl1 As Guna.UI.WinForms.GunaDragControl
+    Friend WithEvents GunaButton1 As Guna.UI.WinForms.GunaButton
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents GunaLabel3 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaLabel2 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaVSeparator1 As Guna.UI.WinForms.GunaVSeparator
+    Friend WithEvents GunaPictureBox1 As Guna.UI.WinForms.GunaPictureBox
+    Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaVSeparator2 As Guna.UI.WinForms.GunaVSeparator
+End Class
---
+++ Example/Example/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Guna.UI.Lib.GraphicsHelper.ShadowForm(Me)
+    End Sub
+
+    Private Sub GunaButton1_Click(sender As Object, e As EventArgs) Handles GunaButton1.Click
+        Process.Start("https://github.com/sobatdata/guna.ui-framework-lib")
+    End Sub
+End Class
